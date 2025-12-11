544766957810861ff5d1503e398e87a7fdfad5ae
Jim Cortez <jim@jimcortez.com>
2025-12-11 14:26:29 -0800

    Fix Windows: add missing POSIX type definitions for sys/stat.h
    - Added dev_t, ino_t, and mode_t type definitions for Windows
    - These types are required by Windows sys/stat.h but aren't provided by default
    - Fixes compilation errors where st_dev, st_ino, etc. were undeclared
    - Windows sys/stat.h now has all the types it needs to compile

--- src/compat/sys/types.h
+++ src/compat/sys/types.h
@@ -3,14 +3,36 @@
 #define SYS_TYPES_H
 
 #ifdef _WIN32
-#include <cstddef>
-#include <cstdint>
+// On Windows, we need to provide types that sys/stat.h and other headers expect
+// Windows doesn't provide all POSIX types by default, so we define them here
+
+// Include Windows base headers that provide fundamental types
+#include <basetsd.h>
+#include <stddef.h>
+
+// Define POSIX types that Windows sys/stat.h expects but aren't provided by default
+// These are needed to prevent compilation errors in sys/stat.h
+#ifndef _DEV_T_DEFINED
+#define _DEV_T_DEFINED
+typedef unsigned int dev_t;
+#endif
+
+#ifndef _INO_T_DEFINED
+#define _INO_T_DEFINED
+typedef unsigned short ino_t;
+#endif
+
+#ifndef _MODE_T_DEFINED
+#define _MODE_T_DEFINED
+typedef unsigned short mode_t;
+#endif
 
 // Windows doesn't have ssize_t, but it's commonly used in POSIX code
-// ssize_t is a signed size type, typically the same as long or int
-// On Windows, we'll use ptrdiff_t (signed version of size_t) or int
-// Since Windows socket functions return int, we'll use int for compatibility
+// Define it here for compatibility
+#ifndef _SSIZE_T_DEFINED
+#define _SSIZE_T_DEFINED
 typedef int ssize_t;
+#endif
 
 #else
 // On Unix/Linux/macOS/web, include the real sys/types.h using include_next
