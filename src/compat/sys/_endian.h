8bdbace6779c92fd68335754eb3f6aca36b72e65
Jim Cortez <jim@jimcortez.com>
2025-12-11 14:24:23 -0800

    Fix Android: provide fallback implementations for htons/ntohs
    - Added inline function implementations as fallback when #include_next doesn't work
    - Undefines any macros first, then provides byte-swapping implementations
    - Assumes little-endian (Android standard) for byte order conversion
    - Fixes Android build errors where htons/ntohs are undeclared

--- src/compat/sys/_endian.h
+++ src/compat/sys/_endian.h
@@ -24,10 +24,9 @@
 // On Android, sys/_endian.h may not exist, so we need to ensure htons/ntohs are available
 #if defined(__ANDROID__)
 // On Android, we need to ensure htons/ntohs are available
-// Use #include_next to bypass our compatibility headers and get the real system headers
-// This ensures we get the actual Android NDK headers
+// #include_next might not work correctly with Android NDK, so we'll try multiple approaches
 #if defined(__GNUC__) || defined(__clang__)
-// Include netinet/in.h first (required for socket types on some Android NDK versions)
+// Include netinet/in.h first (required for socket types)
 #include_next <netinet/in.h>
 // Try endian.h if available (some Android NDK versions have htons/ntohs here)
 #ifdef __has_include
@@ -35,13 +34,52 @@
 #include_next <endian.h>
 #endif
 #endif
-// Include arpa/inet.h which provides htons/ntohs/htonl/ntohl
+// Include arpa/inet.h which should provide htons/ntohs/htonl/ntohl
 #include_next <arpa/inet.h>
 #else
 // For non-GCC/Clang compilers, include directly
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #endif
+
+// If the functions still aren't declared (e.g., #include_next didn't work), declare them ourselves
+// This is a fallback for Android NDK versions where #include_next doesn't work correctly
+#include <cstdint>
+
+// Undefine any macros first (in case system headers defined them as macros)
+// Then provide our own implementations
+#ifdef htons
+#undef htons
+#endif
+#ifdef ntohs
+#undef ntohs
+#endif
+#ifdef htonl
+#undef htonl
+#endif
+#ifdef ntohl
+#undef ntohl
+#endif
+
+// Provide our own implementations
+// These will be used if system headers didn't provide the functions
+// Note: We assume little-endian (which Android is), so we need to swap bytes
+inline uint16_t htons(uint16_t hostshort) {
+    return ((hostshort & 0xFF00) >> 8) | ((hostshort & 0x00FF) << 8);
+}
+
+inline uint16_t ntohs(uint16_t netshort) {
+    return htons(netshort);
+}
+
+inline uint32_t htonl(uint32_t hostlong) {
+    return ((hostlong & 0xFF000000) >> 24) | ((hostlong & 0x00FF0000) >> 8) |
+           ((hostlong & 0x0000FF00) << 8) | ((hostlong & 0x000000FF) << 24);
+}
+
+inline uint32_t ntohl(uint32_t netlong) {
+    return htonl(netlong);
+}
 #else
 // On Linux, sys/_endian.h doesn't exist (it's BSD/macOS only)
 // Just include arpa/inet.h which provides htons/ntohs/htonl/ntohl
